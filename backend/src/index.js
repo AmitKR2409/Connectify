c90f3155fb602bfb039ef4ac28f1b781e92318ce
Amit Kumar <kashyapamit8877@gmail.com>
2026-06-22 18:54:41 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -64,7 +64,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://connectify-ffzi.onrender.com"],
+    origin: ["http://localhost:5173", "https://connectify-amber-one.vercel.app"],
     credentials: true,
   })
 );
